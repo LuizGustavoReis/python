886f65e222c45e7968b830e3428fcad4e047efca
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-20 15:42:28 -0300

    tipos de variavel

--- desafio3.py
+++ desafio3.py
@@ -3,3 +3,4 @@ n2=int(input('segundo numero'))
 soma= n1 + n2
 
 print ('a soma entre {} e {} vale {} '.format(n1, n2, soma))
+
---
+++ exercicio4.py
@@ -0,0 +1,13 @@
+
+
+
+n = input('digite algo')
+
+print('o tipo primitivo é',type(n))
+print('so tem espaços?',n.isspace())
+print('e um numero?',n.isnumeric())
+print('e alfabetico?',n.isalpha())
+print('ele e alfabetico e numerico?',n.isalnum())
+print('esta em maiusculas?',n.isupper())
+print('esta em minuscular',n.islower())
+print('esta capitalizado?',n.istitle())

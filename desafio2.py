70224ac3e46cb9c006f962a52b7720ad6dc3602a
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-20 14:57:28 -0300

    mudando de pasta

---
+++ desafio2.py
@@ -0,0 +1,4 @@
+dia=input('que dia vc nasceu?')
+mes=input('o mes que vc nasceu?')
+ano=input('o ano que vc nasceu?')
+print('voce nasceu em ',dia,'de',mes,'de',ano,'correto?')
---
+++ desafio3.py
@@ -0,0 +1,5 @@
+n1=input('primeiro numero')
+n2=input('segundo numero')
+soma=int(n1) + int(n2)
+
+print ('a soma dos numeros imformado é',soma)

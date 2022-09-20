419e0161c3d45b67007c9980a47e003abdb2a4e4
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-20 15:01:08 -0300

    somando numeros

--- desafio3.py
+++ desafio3.py
@@ -1,5 +1,5 @@
-n1=input('primeiro numero')
-n2=input('segundo numero')
-soma=int(n1) + int(n2)
+n1=int(input('primeiro numero'))
+n2=int(input('segundo numero'))
+soma= n1 + n2
 
 print ('a soma dos numeros imformado é',soma)

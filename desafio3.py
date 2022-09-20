5789894646467cec9be5e951ec5297eafb669670
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-20 15:19:32 -0300

    format

--- desafio3.py
+++ desafio3.py
@@ -2,4 +2,4 @@ n1=int(input('primeiro numero'))
 n2=int(input('segundo numero'))
 soma= n1 + n2
 
-print ('a soma dos numeros imformado é',soma)
+print ('a soma entre {} e {} vale {} '.format(n1, n2, soma))

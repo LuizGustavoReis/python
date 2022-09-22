0e235c1df94c0613db7db2ff41cae524650450cc
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-22 17:42:00 -0300

    erro

--- modolos.py
+++ modolos.py
@@ -2,4 +2,4 @@ from math import sqrt
 
 num=int(input('digite um numero'))
 raiz = sqrt(num)
-print ('a raiz de {} é igual a {}'.format(num,math.floor(raiz)))
+print ('a raiz de {} é igual a {}'.format(num,sqrt(raiz)))

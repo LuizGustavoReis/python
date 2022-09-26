7dbf76f7be8f5a3cba5aa2b4cfc1bda92ce5333a
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-26 18:09:29 -0300

    condiçoes

---
+++ condicoes.py
@@ -0,0 +1,8 @@
+tempo = int(input("quantos anos vc tem o carro:"))
+
+if tempo <=3:
+    print("seu carro e novo tem apenas{} de uso ".format(tempo))
+else:
+    print("seu carro ja é bem usado tem {} anos de uso ".format(tempo))
+
+

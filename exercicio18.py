b7d01c2cca10a19390f88ec26d5af386175064b6
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-09-28 16:45:44 -0300

    exercicio

---
+++ exercicio18.py
@@ -0,0 +1,16 @@
+#for c in range(10,0,-1):
+ #   print (c)
+
+#somente os pares 
+#soma =0
+#for c in range (1,501, 2):
+    #if c %2 == 0:
+    ##elif c %3 == 0:
+      #  soma= soma + c
+
+#print("a soma de todos os valores e {}".format(soma))
+tabuada = 2
+for i in range(1,11):
+    result = tabuada*i
+    print(tabuada,*"x",i,"=",result)
+    
